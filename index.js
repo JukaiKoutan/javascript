c4816c9af6f42059d9c8c418f1c5356f46041add
Christian-Rios <fernandorios856@gmail.com>
2023-07-18 03:22:37 -0300

    pushing

---
+++ index.js
@@ -0,0 +1,8 @@
+var mensaje = "Hola! Mi nombre es Christian y tengo  23 anios"
+var numero = 132134124
+var edad = 10
+var miEdadEs = "son 10"
+var conclusion = edad + "" + miEdadEs
+var cuadroDeDialogo = prompt("Ingresa tu los 8 numeros de tu tarjeta flaco")
+
+console.log(conclusion)
